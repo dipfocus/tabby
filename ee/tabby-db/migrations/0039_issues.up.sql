812ad1855e5de8cae3178b2cb12b25a2ec8c34e2
dipfocus <dipfocus@gmail.com>
2024-09-25 10:40:17 +0800

    create table issues

---
+++ ee/tabby-db/migrations/0039_issues.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE issues(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  source_id VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  CONSTRAINT `idx_url` UNIQUE(`url`)
+);
